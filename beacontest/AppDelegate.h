0dcc90688ba6d02cad72a6a4c42b39f345092a92
cayeric <cayeric@users.noreply.github.com>
2018-02-25 16:32:44 +0100

    Initial Commit

---
+++ beacontest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  beacontest
+//
+//  Created by Cay-Eric Schimanski on 25.02.18.
+//  Copyright © 2018 endform.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
